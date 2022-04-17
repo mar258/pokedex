0a2c620945c4c43282c2c1f12dfa13918e252b54
Maria <pham.maria6@gmail.com>
2022-04-17 14:57:52 -0500

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Maria Pham on 4/17/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
